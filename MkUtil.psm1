286d26628ca0a577fe9846181a7fba59ab15b9b7
Sarayu <Sarayu@DESKTOP-0FQVAL8>
2018-03-24 20:22:51 -0400

    added files

---
+++ MkUtil.psm1
@@ -0,0 +1,42 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-WebStatus
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([string])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true
+                   )]
+        $WebSiteName       
+    )
+
+   
+    Process
+    {
+    $Response = Invoke-WebRequest -Uri $WebSiteName -UseBasicParsing
+
+    if($Response.StatusCode -eq 200)
+    {
+        return "SUCCESS";
+    }
+    else
+    {
+        return "FAILURE";
+    }    
+ 
+
+    }
+   
+}
